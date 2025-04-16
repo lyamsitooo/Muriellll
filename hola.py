c111ce898506586711963f37d22204db1309007b
lyamsitooo <michelinaaxd@gmail.com>
2025-04-16 16:40:44 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,23 @@
+#Nombre="Lyam
+# edad=22"
+
+#print("hola", nombre, "y su edad es", edad)
+
+#print("Ingrese su nombre")
+#nombre=input()
+#print("Ingrese su edad")
+#edad=input()
+#print("hola su nombre es,", nombre, "y su edad es", edad)
+
+print("Ingrese 3 numeros")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+
+print("El promedio de los numeros es",(n1+n2+n3)/3)
+
+if prom>=40:
+    print("el alumno aprobo")
+else:
+    print("el alumno reprobo")
+
